e3d10a261d0e6208a1b0f3f3617a851e1c68d2c3
Kent Wang <yqwang@cheweilai.cn>
2019-04-14 22:40:03 +0800

    add iter_coroutine coroutine

---
+++ test_iter_coroutine/test_coroutine.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+import unittest
+import datetime
+import heapq
+import types
+import time
+
+
+class CoroutineTest(unittest.TestCase):
+    class Task:
+        # def __init__(self, wait_until, coro):
+        #     self.coro = coro
+        #     self.waiting_until = wait_until
+        #
+        # def __eq__(self, other):
+        #     return self.waiting_until == other.waiting_until
+        #
+        # def __lt__(self, other):
+        #     return self.waiting_until < other.waiting_until
+        def __init__(self, wait_until, coro):
+            self.waiting_until = wait_until
+            self.coro = coro
+
+        def __eq__(self, other):
+            return self.waiting_until == other.waiting_until
+
+        def __lt__(self, other):
+            return self.waiting_until < other.waiting_until
+
+    class SleepLoop:
+        # def __init__(self, *coros):
+        #     self._new = coros
+        #     self._waiting = []
+        #
+        # def run_until_complete(self):
+        #     for coro in self._new:
+        #         wait_for = coro.send(None)
+        #         heapq.heappush(self._waiting, CoroutineTest.Task(wait_for, coro))
+        #
+        #     while self._waiting:
+        #         now = datetime.datetime.now()
+        #         task = heapq.heappop(self._waiting)
+        #         if now < task.waiting_until:
+        #             delta = task.waiting_until - now
+        #             time.sleep(delta.total_seconds())
+        #             now = datetime.datetime.now()
+        #         try:
+        #             wait_until = task.coro.send(now)
+        #             heapq.heappush(self._waiting, CoroutineTest.Task(wait_until, task.coro))
+        #         except StopIteration:
+        #             pass
+        def __init__(self, *coros):
+            self._new = coros
+            self._waiting = []
+
+        def run_until_complete(self):
+            for coro in self._new:
+                wait_until = coro.send(None)
+                heapq.heappush(self._waiting, CoroutineTest.Task(wait_until, coro))
+
+            while self._waiting:
+                task = heapq.heappop(self._waiting)
+                now = datetime.datetime.now()
+                if now < task.waiting_until:
+                    delta = task.waiting_until - now
+                    time.sleep(delta.total_seconds())
+                    now = datetime.datetime.now()
+                try:
+                    wait_until = task.coro.send(now)  # todo check
+                    heapq.heappush(self._waiting, CoroutineTest.Task(wait_until, task.coro))
+                except StopIteration:
+                    pass
+
+    @staticmethod
+    @types.coroutine
+    def sleep(seconds):
+        # now = datetime.datetime.now()
+        # wait_until = now + datetime.timedelta(seconds=seconds)
+        # actual = yield wait_until
+        # return actual - now
+        now = datetime.datetime.now()
+        wait_until = now + datetime.timedelta(seconds=seconds)
+        actual = yield wait_until
+        return actual - now
+
+    @staticmethod
+    async def countdown(label, length, *, delay=0):
+        # print(label, 'waiting', delay, 'seconds before starting countdown')
+        # delta = await CoroutineTest.sleep(delay)
+        # print(label, 'starting after waiting', delta)
+        # while length:
+        #     print(label, 'T-minus', length)
+        #     waited = await CoroutineTest.sleep(1)
+        #     length -= 1
+        # print(label, 'lift-off!')
+        print(label, 'waiting', delay, 'before starting')
+        delta = await CoroutineTest.sleep(delay)
+        print(label, 'start after waiting', delta)
+
+        while length:
+            print(label, length)
+            waited = await CoroutineTest.sleep(1)
+            length -= 1
+
+        print(label, 'done')
+
+    def test_countdown(self):
+        loop = CoroutineTest.SleepLoop(CoroutineTest.countdown('A', 5),
+                                       CoroutineTest.countdown('B', 3, delay=2),
+                                       CoroutineTest.countdown('C', 4, delay=1))
+        start = datetime.datetime.now()
+        loop.run_until_complete()
+        print('Total elapsed time is', datetime.datetime.now() - start)
