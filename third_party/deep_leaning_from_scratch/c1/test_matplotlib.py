43071e51e68f5b3e68fd83d8d85c4598276a3ae5
Kent Wang <yqwang@cheweilai.cn>
2019-05-18 10:47:10 +0800

    update ml

---
+++ third_party/deep_leaning_from_scratch/c1/test_matplotlib.py
@@ -0,0 +1,24 @@
+import unittest
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class MatplotlibTest(unittest.TestCase):
+    def test_sin(self):
+        x = np.arange(0, 6, 0.1)
+        y = np.sin(x)
+        plt.plot(x, y)
+        plt.show()
+
+    def test_cos(self):
+        x = np.arange(0, 6, 0.1)
+        y1 = np.sin(x)
+        y2 = np.cos(x)
+
+        plt.plot(x, y1, label='sin')
+        plt.plot(x, y2, linestyle='--', label='cos')
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title('sin & cos')
+        plt.legend()
+        plt.show()
--- third_party/deep_leaning_from_scratch/c1/test_numpy.py
+++ third_party/deep_leaning_from_scratch/c1/test_numpy.py
@@ -25,3 +25,25 @@ class NunPyTest(unittest.TestCase):
         b = np.array([[3, 0], [0, 6]])
         print(a + b)
         print(a * b)
+
+        print(a * 10)
+        print('====================')
+
+        a = np.array([[1, 2], [3, 4]])
+        b = np.array([10, 20])
+        print(a * b)
+        print('====================')
+
+        x = np.array([[51, 55], [14, 19], [0, 4]])
+        print(x)
+        print(x[0])
+        print(x[0][1])
+
+        for row in x:
+            print(row)
+
+        x = x.flatten()
+        print(x)
+        print(x[np.array([0, 2, 4])])
+        print(x > 15)
+        print(x[x > 15])
