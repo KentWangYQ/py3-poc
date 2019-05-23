68283356673bad0f70c1d1c69d6dd57b75e404f8
Kent Wang <sdwyq1238@163.com>
2019-05-23 23:13:10 +0800

    update dlfc

--- DeepLearningFromScratch/common.py
+++ DeepLearningFromScratch/common.py
@@ -7,3 +7,18 @@ def sigmoid(x):
 
 def identity_function(a):
     return a
+
+
+def softmax(a):
+    """
+    softmax 输出层的激活函数，一般用于分类问题
+    输出结果总和为1，体现为概率。
+    :param a:
+    :return:
+    """
+    c = np.max(a)
+    exp_a = np.exp(a - c)  # 溢出对策
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y
--- DeepLearningFromScratch/neural_network.py
+++ DeepLearningFromScratch/neural_network.py
@@ -4,6 +4,12 @@ from .common import *
 
 class ThreeLayerNeuralNetwork:
     def init_network(self):
+        """
+        初始化神经网络
+        包括权重和重置
+        按照神经网络的实现惯例，只把权重记为大写字母W，其他的(偏置或中间结果等)都用小写字母表示。
+        :return:
+        """
         network = {
             'W1': np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]]),
             'b1': np.array([0.1, 0.2, 0.3]),
@@ -16,6 +22,14 @@ class ThreeLayerNeuralNetwork:
         return network
 
     def forward(self, network, x):
+        """
+        前向处理
+        封装了将输入信号转换为输出信号的处理过程
+        前向表示从输入到输出方向的传递处理
+        :param network:
+        :param x:
+        :return:
+        """
         W1, W2, W3 = network['W1'], network['W2'], network['W3']
         b1, b2, b3 = network['b1'], network['b2'], network['b3']
         a1 = np.dot(x, W1) + b1
